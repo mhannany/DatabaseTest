8bda2da9eca12fbe0f10538c57aee6fd95119c3b
Mirna Garcia <mhannany@gmail.com>
2023-07-26 14:33:52 -0700

    Initial commit

---
+++ nameSeed.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+//Database
+const database = (module.exports = () => {
+  const connectionParams = {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    mongoose.connect(
+      "mongodb+srv://mhannany:LabTest2023@cluster0.25bsjzc.mongodb.net/Exams23002"
+    );
+    console.log("Data connected succesfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Database connection failed");
+  }
+});
+
+//creating schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  sid: Number,
+});
+
+// Create a model based on the schema
+const Student = mongoose.model("Student", studentSchema, "examRecords");
+
+async function createNewStudent() {
+  try {
+    const newStudent = new Student({
+      name: "Mirna Garcia",
+      sid: 300367454,
+    });
+
+    const createdStudent = await newStudent.save();
+    console.log("New Student created:", createdStudent);
+
+    // Closing the connection: good practice
+    mongoose.connection.close();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+app.listen(3000);
+console.log("App running http://localhost:3000");
+
+app.use("/", (req, res, next) => {
+  res.json({
+    name: "Mirna Garcia",
+    sid: 300367454,
+  });
+  database();
+  createNewStudent();
+});
